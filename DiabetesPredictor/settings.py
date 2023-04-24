6f617a62c9bb8465d787a2a24babbea919caba51
SimranKachle <simrankachle27@gmail.com>
2023-04-24 06:54:00 +0530

    commit

--- DiabetesPredictor/settings.py
+++ DiabetesPredictor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,6 +56,7 @@ ROOT_URLCONF = 'DiabetesPredictor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        
         'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -126,3 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     ]
 
+# Base url to serve media files  
+MEDIA_URL = '/media/'  
+  
+# Path where media is stored  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
